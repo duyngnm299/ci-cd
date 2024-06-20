cf5546d6700a7608eb761aa5ca7a5c75c6bd5da0
duyngnm299 <duyngnm.299@gmail.com>
2024-06-20 22:36:31 +0700

    add ci-cd

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/config-source/',
+  base: '/ci-cd/',
   server: {
     port: 3000
   },
